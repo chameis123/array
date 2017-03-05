432e20a407782bdab606592b3f2077b331dc2d2b
chameis123 <chameis123@localhost.localdomain>
2017-03-05 17:50:42 -0500

    upated notes

--- 11test.rb
+++ 11test.rb
@@ -1,12 +1,14 @@
 def soda# this is my method soda 
 	soda_types = ["spite","coke","mountain dew","orange soda","dr peper"] #this is soda_types being set to the array 
 	           #   ["spite","coke","mountain dew","orange soda","dr peper"]
+	good_soda = [] # this is another array array2
 		puts soda_types[2]# this is putting the second placeholder
 		puts soda_types[-2]# this this is putting the 4th placeholder 
 	soda_types.each do |soda_names|# this is iterating though my array soda_tpype going though each spot in the my array
-		puts "good soda " + soda_names
-		puts soda_names
+    	good_soda.push(soda_names)
+		puts "good soda " + soda_names # printing good soda + soda_names add good soda to each string in my array 
+        puts good_soda
 	end
 end 
 
-return soda # returns my nethod soda 
+soda # returns my nethod soda 
